9b28008502decca0ca45a6900295d981bfcc8c50
Donovan Richards <richardsd@roverkids.org>
2023-12-11 14:18:49 -0500

    2.4

--- Prototype 2/Assets/Scripts/New Folder/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Scripts/New Folder/DestroyOutOfBounds.cs
@@ -15,11 +15,13 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If an object goes past the players view in the gaem, remove that
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         } else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
